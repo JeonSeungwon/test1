09b5977b9accb0301f9ce0693efbe612b66f6ba7
JeonSeungwon <110527206+JeonSeungwon@users.noreply.github.com>
2022-08-05 16:18:06 +0900

    16:18

--- BusinessLogic.java
+++ BusinessLogic.java
@@ -33,33 +33,59 @@ public class BusinessLogic {
 						Scanner scan = new Scanner(System.in);
 						String choice = scan.nextLine();
 						scan.close();
-
 						if (choice.toUpperCase().equals("Y")) {
 							Boolean reservationable = false;
 							ReservationBook rb = new ReservationBook(stuNo, bookNo);
 							reservationable = rb.checkReservationableStu();
 							if (!reservationable) {
-								System.out.println("도서예약은 3권까지 가능합니다.");
 								rb.reservationCancell();
+								System.out.println("도서예약은 3권까지 가능합니다.");
 							} else {
 								reservationable = rb.checkReservationableBook();
 								if (!reservationable) {
-									System.out.println("해당 도서의 예약인원은 2명까지 가능합니다.");
 									rb.reservationCancell();
+									System.out.println("해당 도서의 예약인원은 2명까지 가능합니다.");
 								} else {
+									reservationable = rb.checkLoanOrRsrv();						
 									if (reservationable) {
-										System.out.println("도서예약이 완료되었습니다.");
+										rb.reservationCancell();
+										System.out.println("학생이 해당 도서를 이미 대여 또는 예약 중 입니다.");
+									} else {
 										rb.reservationConfirm();
+										System.out.println("도서예약이 완료되었습니다.");
 									}
 								}
 							}
 						} else if (choice.equals("N")) {
-							System.out.println("도서 대여 및 예약이 취소되었습니다.");
 							bb.borrowCancell();
+							System.out.println("도서 대여 및 예약이 취소되었습니다.");
 						}
 					}
 				}
 			}
 		}
+	}
+	
+	public void returnBook(int stdNo, int bookNo) throws SQLException {
+		ReturnBook rb = new ReturnBook(stdNo, bookNo);
+		boolean unborrowable = rb.checkUnreturnBook();
+		boolean returnable = rb.checkHaveBook();
+		
+		if (!unborrowable) {
+			if (returnable) {
+				System.out.println("도서반납이 완료되었습니다.");
+				rb.returnConfirm();
+			}
+		} else {
+			unborrowable = rb.checkSuspension();
+			if (unborrowable) {
+				System.out.println("대출정지일자가 갱신되었습니다.");
+				rb.updateSuspension();
+			} else {
+				System.out.println("대출정지일자가 추가되었습니다.");
+				rb.newSuspension();
+			}
+		}
+
 	}
 }
